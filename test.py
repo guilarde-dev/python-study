d96f0d5529999e5f2c5a96d74a1859c3fe24308d
Guilarde | Pythonic Effort <enzoguisanmartin@gmail.com>
2026-04-21 13:36:49 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,103 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
+import logging
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import os
 
-datas = []
+"""
+Script: Mercado Livre (e-commerce) scraper
+Author: guilarde dev
+Date: 21/04/2026
+Description: extracts informations about mouse in the site Mercado Livre and persists in CSV, XLSX and Google Sheets
+"""
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+"""
+CONFIGURATION
+"""
 
-    page = browser.new_page()
+#configurating logging
+logging.basicConfig(
+    level= logging.INFO,
+    format= '%(asctime)s - [%(levelname)s] - %(message)s',
+    handlers= [
+        logging.FileHandler('scrap_mercado_livre.log', encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 
-    page.goto('https://www.mercadolivre.com.br/')
+#path to acess files
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+path_to_creds = os.path.join(BASE_DIR, 'creds.json')
 
-    page.wait_for_selector('#cb1-edit')
+BASE_DIRE = os.path.dirname(os.path.abspath(__file__))
+path_to_config = os.path.join(BASE_DIR, 'config.json')
 
-    page.fill('input[name="as_word"]', 'mouse logitech')
-    page.press('input[name="as_word"]', 'Enter')
+#setup of google sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name(path_to_creds, scope)
+client = gspread.authorize(creds)
+planner = client.open('planner mercado livre').sheet1
 
-    page.wait_for_load_state('networkidle')
-    boxes = page.locator('.poly-card__content').all()
 
-    for box in boxes:
-        try:
-            title = box.locator('.poly-component__title-wrapper').inner_text()
-            price = box.locator('.andes-money-amount__fraction').first.inner_text()
-            clean_price = float(price.replace('R$', ''))
+
+class Scraper:
+    def __init__(self):
+        self.datas = []
+        self.browser = None
+        self.page = None # self.page will be a page of browser
+        self.informations = None #filter of prices
+
+    def open_json(self):
+        #open the filter
+        with open(path_to_config, 'r', encoding='utf-8') as f:
+            self.informations = json.load(f)
+
+    def acess_site(self):
+        with sync_playwright() as p:
+            while True:
+                try:
+                    #configuring browser and acessing website
+                    self.browser = p.chromium.launch(headless=False)
+
+                    self.page = self.browser.new_page()
+
+                    self.page.goto('https://www.mercadolivre.com.br/')
+
+                    self.page.wait_for_selector('#cb1-edit')
+                except
+
+    '''
+    DATA EXTRACTION AND DATA CLEANING
+    '''
+    def take_datas(self):
+            # search for the product and wait the site load
+            self.page.fill('input[name="as_word"]', 'mouse')
+            self.page.press('input[name="as_word"]', 'Enter')
+            self.page.wait_for_load_state('networkidle')
+
+            boxes = self.page.locator('.poly-card__content').all()
+
+            #now extract and clean data
+            for box in boxes:
+                try:
+                    title = box.locator('.poly-component__title-wrapper').inner_text()
+                    price = box.locator('.andes-money-amount__fraction').first.inner_text()
+                    clean_price = float(price.replace('R$', ''))
             
-            product = {
-                'title': title,
-                'price': clean_price
-            }
+                    product = {
+                        'title': title,
+                        'price': clean_price
+                    }
 
-            datas.append(product)
+                    self.datas.append(product)
 
-        except Exception as e:
-            print(f'detected error: {e}')
+                except Exception as e:
+                    print(f'detected error: {e}')
 
     try:
-        df = pd.DataFrame(datas)
+        df = pd.DataFrame(self.datas)
         df.to_excel('test1.xlsx', index=False)
         df.to_csv('test1.csv', encoding='utf-8-sig', sep=';', index=False)
     except Exception as e:
